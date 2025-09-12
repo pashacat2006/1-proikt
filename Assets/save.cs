84b2a28413603c1ee42eedf0e85ca1bec33a7d07
Quark <gennadiy.kaydalov@mail.ru>
2025-09-12 20:50:01 +0300

    Added Save

--- Assets/save.cs
+++ Assets/save.cs
@@ -1,12 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
+public class GameData
+{
+    public Vector3 Position;
+    public float Health;
+}
 
 public class save : MonoBehaviour
 {
-    public GameObject cat;
-    void save1()
+    private string _fileName = "MySave.cat";
+
+    private string _path;
+
+    public GameData player;
+
+    private void Start()
     {
-        PlayerPrefs.Save();
+        _path = Application.persistentDataPath + "/" + _fileName;
+        print("Save path: " + _path);
+
+        player = new GameData();
+
+        player.Position = new Vector3(0, 10, 2);
+        player.Health = 100;
+    }
+
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(player);
+        File.WriteAllText(_path, json);
+    }
+
+    public void Load()
+    {
+        string file = File.ReadAllText(_path);
+
+        player = JsonUtility.FromJson<GameData>(file);
     }
 }
